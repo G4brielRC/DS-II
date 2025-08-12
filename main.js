fc97af4fb995bbd1c613452950774a501376f2a0
G4brielRC <gabriel.crepaldi9@etec.sp.gov.br>
2025-08-12 20:55:00 -0300

    basico JS

---
+++ main.js
@@ -0,0 +1,26 @@
+/*function soma (a,b){
+   return a + b
+}
+
+console.log(soma (836,58))
+*/
+
+/*
+var lista = [1,2,3,4]
+
+console.log (lista)
+console.log (lista[2])
+*/
+
+var lista2 =
+[
+    {
+        "nome" : "sla", "tantofaz" : 2
+    },
+
+    {
+        "nome" : "fds", "pqp" : 3
+    }
+]
+
+console.log(lista2)
